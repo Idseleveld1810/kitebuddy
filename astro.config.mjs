431de87b80716a28c5015810ff854c7c56d3b0e7
Idseleveld1810 <idseleveld1@gmail.com>
2025-09-09 21:18:02 +0200

    Add Tailwind integration back for proper styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  integrations: [react()],
+  integrations: [react(), tailwind()],
 });
